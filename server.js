8cf8d0ecd6d8c3cea21f492fe687e277cb8dabec
Ziaul Haque <ziaulhoque4820@gmail.com>
2026-05-24 16:25:11 +0600

    Database connected successfully with MongoDB

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
+import dotenv from "dotenv";
+dotenv.config();
 import app from "./src/app.js";
+import mongoose from "mongoose";
 
 
-let serverInstance;
+const PORT = process.env.PORT || 5000;
 
-const PORT = 5000;
+const startServer = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("Database connected successfully!");
 
-const startServer = () => {
-    serverInstance = app.listen(PORT, () => {
-        console.log(`Server running at http://localhost:${PORT}`);
-    })
+
+        app.listen(PORT, () => {
+            console.log(`Server running at http://localhost:${PORT}`);
+        })
+    } catch (error) {
+        console.error("Failed to start the server:", error);
+    }
 };
 
 startServer()
