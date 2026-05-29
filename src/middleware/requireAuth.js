5bc8fde75d1a4bc6f748f4e100f2da00a6a2fc3d
Ziaul Haque <ziaulhoque4820@gmail.com>
2026-05-29 14:16:35 +0600

    Create requireAuth for user authentication

---
+++ src/middleware/requireAuth.js
@@ -0,0 +1,38 @@
+export const requireAuth = () => {
+    return async (req, res, next) => {
+        try {
+            const auth = req.app.get("auth");
+
+            if (!auth) {
+                throw new Error("Better Auth instance not found.")
+            }
+
+            const session = await auth.api.getSession({
+                headers: fromNodeHeaders(req.headers),
+            });
+
+            console.log(session);
+
+            if (!session || session.user) {
+                return res.status(401).json({
+                    success: false,
+                    message: "Unauthorized. Please login first.",
+                });
+            }
+
+            req.user = {
+                id: session.user.id,
+                email: session.user.email,
+                name: session.user.name,
+            };
+
+            next();
+        } catch (error) {
+            console.error("Auth Middleware Error:", error);
+            return res.status(401).json({
+                success: false,
+                message: "Session invalid or expired.",
+            });
+        }
+    }
+}
